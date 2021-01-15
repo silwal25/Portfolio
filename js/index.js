dafbad24475d730caf7a5143c9d40d40a7099e72
Lalit Silwal <lalitsilwal.2599@gmail.com>
2021-01-15 22:02:44 +0530

    completed the front page

--- js/index.js
+++ js/index.js
@@ -22,4 +22,4 @@ let parallaxInstance = new Parallax(scene)
 
 //Lander animation
 let t1 = gsap.timeline({ defaults: { duration: ".5", ease: "ease-in" } })
-t1.to(".lander--1", { y: "-100%" }).to(".lander--2", { y: "-100%", delay: "-.35" }).to(".lander--3", { y: "-100%", delay: "-.35" }).to(".lander", { y: "-100%", delay: "-.35" })
+t1.to(".lander--1", { y: "-100%" }).to(".lander--2", { y: "-100%", delay: "-.35" }).to(".lander--3", { y: "-100%", delay: "-.35" }).to(".lander", { y: "-100%", delay: "-.5" }).fromTo(".home__left", { x: "-100%" }, { x: "0", delay: "-.5" }).fromTo(".scene", { x: "100%" }, { x: "0", delay: "-.5" }).fromTo(".header", { opacity: 0 }, { opacity: 1 })
