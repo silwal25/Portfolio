9c853db2c64b1e0d411ec7952361bb4b2b58230d
Lalit Silwal <lalitsilwal.2599@gmail.com>
2021-01-18 11:51:45 +0530

    made loader

--- js/index.js
+++ js/index.js
@@ -1,104 +1,110 @@
 /**
- * //Defining variables
-let home = $(".home")
-let height = $(window).height() - home.offset().top
-home.css("height", height)
- */
-function loader() {
-  let tl = new gsap.timeline({ duration: ".5" })
-  tl.to(".main", { opacity: "0" })
-  tl.to(".loader--first", {
-    scaleY: "1",
-    transformOrigin: "bottom left"
-  })
-  tl.to(
-    ".loader--second",
-    {
-      scaleY: "1",
-      transformOrigin: "top left"
-    },
-    "-=0.5"
-  )
-  tl.to(
-    ".hourglass",
-    {
-      opacity: 1
-    },
-    "-=0.2"
-  )
-  tl.to(".hourglass", {
-    opacity: 0,
-    delay: "1.5"
-  })
-  tl.to(".loader--first", {
+ * //Initial Load animation
+initialLoad()
+function initialLoad() {
+  let tl = new gsap.timeline({ duration: "0.5" })
+  tl.to(".initial-loader--first", {
     scaleY: "0",
     transformOrigin: "top left"
   })
   tl.to(
+    ".initial-loader--second",
+    {
+      scaleY: "0",
+      transformOrigin: "bottom left"
+    },
+
+    "-=0.5"
+  )
+}
+ */
+
+//barba js animations
+
+function initialLoad() {
+  let tl = new gsap.timeline({ duration: "0.5" })
+  tl.fromTo(
+    ".loader--first",
+    {
+      scaleY: "1"
+    },
+    {
+      scaleY: "0",
+      transformOrigin: "top left"
+    }
+  )
+  tl.fromTo(
     ".loader--second",
+    {
+      scaleY: "1"
+    },
     {
       scaleY: "0",
       transformOrigin: "bottom left"
     },
     "-=0.5"
   )
-  tl.to(
-    ".hourglass",
-    {
-      opacity: 0
-    },
-    "-=0.2"
-  )
-  tl.to(".main", { opacity: "1" })
 }
-function loaderIn() {
-  let tl = new gsap.timeline({ duration: ".5" })
-  tl.to(".main", { opacity: "0" })
+
+function loader() {
+  let tl = new gsap.timeline()
+
   tl.to(".loader--first", {
     scaleY: "1",
-    transformOrigin: "bottom left"
+    transformOrigin: "top left"
   })
   tl.to(
     ".loader--second",
     {
       scaleY: "1",
+      transformOrigin: "bottom left"
+    },
+
+    "-=0.5"
+  )
+  tl.to(
+    ".loader--first",
+    {
+      scaleY: "0",
+      transformOrigin: "bottom left"
+    },
+    "0.5"
+  )
+  tl.to(
+    ".loader--second",
+    {
+      scaleY: "0",
       transformOrigin: "top left"
     },
+
     "-=0.5"
   )
 }
 
-function initialLoad() {
-  gsap.to(".initial-loader--first", {
-    scaleY: "0",
-    transformOrigin: "bottom left",
-    delay: "1"
-  })
-  gsap.to(".initial-loader--second", {
-    scaleY: "0",
-    transformOrigin: "top left",
-    delay: "1"
-  })
-  gsap.fromTo(
+function contentAnimationIn() {
+  let tl = new gsap.timeline({ duration: "0.5" })
+  tl.from(
     ".main",
-    { opacity: "0" },
     {
-      opacity: "1",
-      duration: "0.5",
-      delay: "1.75"
-    }
+      y: "-50",
+      opacity: "0"
+    },
+    "0.75"
   )
 }
 
 barba.init({
   transitions: [
     {
-      name: "loader-animation",
+      name: "loading-animation",
       once() {
         initialLoad()
       },
+      leave() {
+        loader()
+      },
       enter() {
-        loaderIn()
+        contentAnimationIn()
       }
     }
   ]
